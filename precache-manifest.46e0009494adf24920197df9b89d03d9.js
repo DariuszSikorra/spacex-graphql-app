fd813c22ac1ea5dcb4cacc0b2a88fd932f5ff767
Darek Sikorra <dariusz.sikorra@gmail.com>
2020-01-16 17:53:08 +0100

    Updates

---
+++ precache-manifest.46e0009494adf24920197df9b89d03d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34d2e1de56bb2eed27979ccd66c39dc7",
+    "url": "/spacex-graphql-app/index.html"
+  },
+  {
+    "revision": "128190e6b6417070377d",
+    "url": "/spacex-graphql-app/static/css/main.1b39a72c.chunk.css"
+  },
+  {
+    "revision": "b8d8bc473b19dec69d9a",
+    "url": "/spacex-graphql-app/static/js/2.879e5f32.chunk.js"
+  },
+  {
+    "revision": "54520d64defcffe4599494e9cead2142",
+    "url": "/spacex-graphql-app/static/js/2.879e5f32.chunk.js.LICENSE"
+  },
+  {
+    "revision": "128190e6b6417070377d",
+    "url": "/spacex-graphql-app/static/js/main.3ab56852.chunk.js"
+  },
+  {
+    "revision": "0df70f6b5a4f5bb222ea",
+    "url": "/spacex-graphql-app/static/js/runtime-main.a7eacd03.js"
+  }
+]);
